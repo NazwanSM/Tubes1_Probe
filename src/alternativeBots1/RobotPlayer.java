56c12a2e4601de4a54ed7da4d614b006087d78fb
NazwanSM <nazwan.siddqi@gmail.com>
2026-03-05 12:25:50 +0700

    feat: initial setup for all runner

---
+++ src/alternativeBots1/RobotPlayer.java
@@ -0,0 +1,64 @@
+package alternativeBots1;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+
+/**
+ * RobotPlayer is the class that describes your main robot strategy.
+ * The run() method inside this class is like your main function: this is what we'll call once your robot
+ * is created!
+ */
+public class RobotPlayer {
+    static int turnCount = 0;
+
+    static final Random rng = new Random(6147);
+
+    /** Array containing all the possible movement directions. */
+    static final Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
+
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+    }
+
+    /**
+     * Run a single turn for towers.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    public static void runTower(RobotController rc) throws GameActionException{
+
+    }
+
+
+    /**
+     * Run a single turn for a Soldier.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    public static void runSoldier(RobotController rc) throws GameActionException{
+    }
+
+
+    /**
+     * Run a single turn for a Mopper.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    public static void runMopper(RobotController rc) throws GameActionException{
+    }
+
+    public static void runSplasher(RobotController rc) throws GameActionException{
+    }
+
+    public static void updateEnemyRobots(RobotController rc) throws GameActionException{
+    }
+}
+
