f5e338c9d3ac86688d9305f9d335301c1100478f
NazwanSM <nazwan.siddqi@gmail.com>
2026-03-11 01:39:36 +0700

    feat: combat class (Alt bot 1)

---
+++ src/alternativeBots1/Combat.java
@@ -0,0 +1,28 @@
+package alternativeBots1;
+import battlecode.common.*;
+
+public class Combat {
+
+    static void towerAttack(RobotController rc) throws GameActionException {
+        if (!rc.isActionReady()) return;
+
+        RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+        if (enemies.length == 0) return;
+
+        RobotInfo bestTarget = null;
+        int lowestHP = Integer.MAX_VALUE;
+
+        for (RobotInfo enemy : enemies) {
+            if (rc.canAttack(enemy.getLocation())) {
+                if (enemy.getHealth() < lowestHP) {
+                    lowestHP = enemy.getHealth();
+                    bestTarget = enemy;
+                }
+            }
+        }
+
+        if (bestTarget != null) {
+            rc.attack(bestTarget.getLocation());
+        }
+    }
+}
